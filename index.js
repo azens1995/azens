0efd8b2f5f60e90004ec27fec5be501886f10769
Eklak Dangaura <eklakdangaura@Perls-MacBook-Pro.local>
2022-12-14 22:26:23 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+console.log(`
+Eklak Dangaura - Software Engineer
+
+Hi, this is Eklak. I am a Software Engineer from Nepal.
+With 6+ years of experience in various technology, I am now learning
+to create a CLI application.
+
+If you want to connect with me, you can find me here:
+
+Twitter: https://twitter.com/perleklak
+Github: https://githuib.com/azens1995
+`);
